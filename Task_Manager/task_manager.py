541851dc4c5bc4f9d6adb3879512cd881b924cc4
cj-byte-creator <cjgonito@gmail.com>
2026-04-06 16:57:45 +0800

    Improved code logic in while loop and case checking

--- Task_Manager/task_manager.py
+++ Task_Manager/task_manager.py
@@ -22,41 +22,33 @@ print("-----Welcome to the Task Manager!-----")
 print("You can : \n 1. Add Task \n 2. View all Tasks \n 3. Mark tasks as done \n 4. Exit \n")
 
 while True:
-    with open("Task_Manager/tasks.json","w") as f:
-        json.dump(tasks,f,indent=4)
     choice = input("Input the number of your choosing : ")
     match choice:
         case "1":#Adding tasks
             task_name = input("Input task title/name : ")
-            task = Task(task_name)
-            tasks.append(task.to_dict())
+            new_task = Task(task_name)
+            tasks.append(new_task.to_dict())
+            with open("Task_Manager/tasks.json","w") as f:
+                json.dump(tasks,f,indent=4)
             print("Task added!")
-            continue
         case "2":#Printing all tasks
             if tasks == []:
                 print("No Tasks yet")
-                continue
             else:
                 print("---Tasks---")
-                i = 1
-                for task in tasks:
-                    print(f"{i}. {task["title"]} {"[ / ]" if {task["is_completed"]} == True else "[  ]"}")
-                    i += 1
-                continue
+                for i, task in enumerate(tasks,1):
+                    status = "[ / ]" if task["is_completed"] else "[  ]"
+                    print(f"{i}. {task["title"]} {status}")
         case "3":#Marking tasks as done
             print("Not Implemented Yet!")
-            continue
         case "4":#Exiting the program
+            with open("Task_Manager/tasks.json","w") as f:
+                json.dump(tasks,f,indent=4)
             break
         case _:#Invalid Input Checker
             print("-----Write a valid number.-----")
-            continue
 
 print("-----Thank you!-----")
-
-
-# print(user_list)
-
 
 
 
